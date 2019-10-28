e326ddafce97c815aa2a7badc35b7e2031a44442
georgiev84 <cska30>
2019-10-28 23:11:52 +0200

    Initial commit

---
+++ src/main/java/app/CodeFirstApplication.java
@@ -0,0 +1,16 @@
+package app;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class CodeFirstApplication {
+    public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("soft_uni");
+
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        Runnable engine = new Engine(entityManager);
+        engine.run();
+    }
+}
